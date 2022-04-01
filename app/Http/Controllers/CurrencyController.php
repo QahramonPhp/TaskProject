b8ecad573516bef41f95ba40266c6317bb9422a7
maxkamov <qahramonmaxkamov01@gmail.com>
2022-04-01 12:13:15 +0300

    modified controller to project

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -22,6 +22,6 @@ class CurrencyController extends Controller
                 ]);
             }
         }
-        return veiw('test', ['currency' ->$result]);
+        return 'success';
     }
 }
